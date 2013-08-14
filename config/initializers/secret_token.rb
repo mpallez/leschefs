80710b35e3a8f9d1135fa1df753a112e576c36a8
Maxime Pallez <maxime.pallez@esial.net>
2013-08-14 21:05:01 +0200

    First commit. Ajout des fichiers Rails du projet leschefs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leschefs::Application.config.secret_key_base = '63e2c1bf5aa87cf75016c2d109debcae52ae503957ba60a895b376e5065fe9f37a5e201169363c5f7c5cd5d4eb258ec69181e954fc2e7b3789bde26a4e56d3e4'
